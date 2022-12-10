f0b848bd7961628a018c9de9d3aeac7a9ef77400
jandersolutions <jba1719@gmail.com>
2022-12-10 11:51:06 -0300

    initial commit

---
+++ script-local.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Criando as imagens..."
+
+docker build -t jandersolutions/projeto-k8s-backend:1.0/.
+docker build -t jandersolutions/projeto-k8s-database:1.0/.
+
+echo "Realizando push..."
+
+docker push jandersolutions/projeto-k8s-backend:1.0
+docker push jandersolutions/projeto-k8s-database:1.0
+
+echo "Criando serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando deployments..."
+
+kubectl apply -f ./deployment-local.yml
+
+minikube service php --url
+minikube tunnel --cleanup
